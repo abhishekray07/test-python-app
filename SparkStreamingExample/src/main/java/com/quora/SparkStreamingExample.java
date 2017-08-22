a9c941dceebfa9e87b0238a6bad189435878a46f
Abhishek Ray <abhishekray07@gmail.com>
2017-08-21 19:33:35 -0700

    add spark streaming example

---
+++ SparkStreamingExample/src/main/java/com/quora/SparkStreamingExample.java
@@ -0,0 +1,67 @@
+package com.quora;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaPairReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka.KafkaUtils;
+import scala.Tuple2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public final class SparkStreamingExample {
+
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    private SparkStreamingExample() {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length < 5) {
+            System.err.println("Usage: JavaKafkaWordCount <zkQuorum> <group> <topics> <numThreads> <hdfsLocation>");
+            System.exit(1);
+        }
+
+        // Create spark conf
+        SparkConf sparkConf = new SparkConf().setAppName("SparkStreamingExample");
+
+        // Create the context with 2 seconds batch size
+        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, new Duration(2000));
+
+        int numThreads = Integer.parseInt(args[3]);
+        Map<String, Integer> topicMap = new HashMap<>();
+        String[] topics = args[2].split(",");
+        for (String topic: topics) {
+            topicMap.put(topic, numThreads);
+        }
+
+        JavaPairReceiverInputDStream<String, String> messages =
+                KafkaUtils.createStream(jssc, args[0], args[1], topicMap);
+
+        JavaDStream<String> lines = messages.map(Tuple2::_2);
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(SPACE.split(x)).iterator());
+        JavaPairDStream<String, Integer> wordCounts = words.mapToPair(s -> new Tuple2<>(s, 1))
+                .reduceByKey((i1, i2) -> i1 + i2);
+
+        wordCounts.print();
+
+        String hdfsPath = args[4];
+        wordCounts.saveAsHadoopFiles(hdfsPath, "", Text.class, IntWritable.class, TextOutputFormat.class);
+
+        jssc.start();
+        jssc.awaitTermination();
+
+    }
+}
